ec4a180b1a2d0ef4d2ff5e2c3cd84c8619ae8619
UEFI-code <zhuoshizhang@hotmail.com>
2024-08-18 05:24:13 +0000

    Add UserMode Interrupt Tester

---
+++ UserMode_InterruptTester/int.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<Windows.h>
+
+int main()
+{
+    UINT8 *exe = (UINT8 *)VirtualAlloc(NULL, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (exe == NULL)
+    {
+        printf("VirtualAlloc Failed!\n");
+        return -1;
+    }
+    exe[0] = 0xCD; // INT 0xXX
+    exe[2] = 0xC3; // RET
+
+    UINT8 INT_num = 0;
+
+    while(1)
+    {
+        printf("Enter Interrupt Number: ");
+        scanf("%x", &INT_num);
+        exe[1] = INT_num;
+        ((void(*)())exe)();
+    }
+}
