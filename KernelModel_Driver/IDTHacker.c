09291f8bb4e36775dbd8e0a1f57ddf4ad6d87f7d
UEFI-code <zhuoshizhang@hotmail.com>
2024-08-25 01:40:39 +0000

    Remove Useless Code

--- KernelModel_Driver/IDTHacker.c
+++ KernelModel_Driver/IDTHacker.c
@@ -12,7 +12,6 @@ void RegisterMyINTHandler()
 {
     // trigger breakpoint
     DbgBreakPoint();
-    //UINT8 *showMem = ExAllocatePool2(POOL_FLAG_NON_PAGED, 0x1000, '2333');
     HackIDT_FireAndForget2(0x78, myINTHandler);
     TestINT();
 }
