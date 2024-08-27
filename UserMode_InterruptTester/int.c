f3d61ac770abee2c1947275ae76a3e85453fe6e2
UEFI-code <zhuoshizhang@hotmail.com>
2024-08-27 09:30:01 +0000

    Try New Methd

--- UserMode_InterruptTester/int.c
+++ UserMode_InterruptTester/int.c
@@ -9,8 +9,20 @@ int main()
         printf("VirtualAlloc Failed!\n");
         return -1;
     }
+    memset(exe, 0x90, 0x1000); // NOP
     exe[0] = 0xCD; // INT 0xXX
     exe[2] = 0xC3; // RET
+
+    UINT8 *exe_for_ring0 = (UINT8 *)VirtualAlloc(NULL, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (exe_for_ring0 == NULL)
+    {
+        printf("VirtualAlloc Failed!\n");
+        return -1;
+    }
+    memset(exe_for_ring0, 0x90, 0x1000); // NOP
+    exe_for_ring0[4] = 0xF4; // HLT
+    exe_for_ring0[8] = 0xEB; // JMP SHORT 
+    exe_for_ring0[9] = 0xFA; // -5 from here
 
     UINT8 INT_num = 0;
 
@@ -21,5 +33,10 @@ int main()
         printf("Triggering Interrupt 0x%X\n", INT_num);
         exe[1] = INT_num;
         ((void(*)())exe)();
+        if (INT_num == 0x78)
+        {
+            // Goto the loop to prevent calling NT API
+            ((void(*)())exe_for_ring0)();
+        }
     }
 }
