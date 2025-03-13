629306bb1d1f8fb6fcb2eca9f0253e9a5c2a6437
UEFI-code <zhuoshizhang@hotmail.com>
2025-03-13 18:17:55 +0900

    Nice Try to Avoid BSOD

--- UserMode_InterruptTester/asm_gadget.s
+++ UserMode_InterruptTester/asm_gadget.s
@@ -10,11 +10,22 @@ trigger_int_79h PROC
     ; Trigger an interrupt
     int 079h
     ; Now We are Ring0!
-    swapgs; WinDbg need a "right" gs to work...
-    int 3; Inform windbg
-    nop_start:
+    ; swapgs; WinDbg need a "right" gs to work...
+    ; int 3; Inform windbg
     dq 9090909090909090h
-    jmp nop_start
+    ; Well, we already played enough, so let's do something pretty...
+    ; You are right, we need back to Ring3...
+    mov rax, rsp ; Remember the current stack pointer
+    push 02bh ; SS
+    push rax ; RSP
+    pushfq ; RFLAGS
+    push 033h ; CS
+    mov rax, return_addr
+    push rax ; Return address
+    iretq
+    return_addr:
+    ; OK, we backed to Ring3
+    int 3; Inform windbg
     ret
 trigger_int_79h ENDP
 
